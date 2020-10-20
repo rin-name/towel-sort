0eff5b94b16ed110f02887efa4ba82d53972cf8a
rin-name <marinka_kostyuk@inbox.ru>
2020-10-20 11:13:28 +0300

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let result = [];
+    if ((!matrix) || (matrix.length === 0)) {
+        return [];
+    } else {
+
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 === 1 || i === 1) {
+                result.push(matrix[i].reverse());
+            } else {
+                result.push(matrix[i]);
+            }
+        }
+    }
+    return result.flat();
 }
+
+
+
